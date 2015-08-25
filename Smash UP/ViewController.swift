f7009ca53a68b728c572d9f243ff48ca16f1eb88
alec fullmer <alecfullmer@alec-fullmers-MacBook-Pro.local>
2015-08-24 22:00:28 -0600

    Initial Commit

---
+++ Smash UP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smash UP
+//
+//  Created by alec fullmer on 8/24/15.
+//  Copyright (c) 2015 alec fullmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
